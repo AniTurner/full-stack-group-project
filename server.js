b09847ea67c4d1feb4fc3d8245a9895ba549da17
Ani Turner <aniturner93@gmail.com>
2019-03-19 21:43:00 -0600

    completing first part

--- server.js
+++ server.js
@@ -1,30 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const expressJwt = require("express-jwt");
+const morgan = require('morgan')
+const PORT = process.env.PORT || 7000;
 const app = express();
 require("dotenv").config();
 
 
-
-
-
-//Bodyparser Middleware
+// Middlewares for every request
 app.use(express.json())
+app.use(morgan('dev'))
 app.use("/auth", require("./routes/auth"));
+app.use("/todo", require("./routes/todo.js"));
+
+// Make the app use the express-jwt authentication middleware on anything starting with "/api"
+app.use("/api", expressJwt({secret: process.env.SECRET}));
+
+// Add `/api` before your existing `app.use` of the todo routes.
+// This way, it must go through the express-jwt middleware before
+// accessing any todos, making sure we can
+app.use("/api/todo", require("./routes/todo"));
 
 
-//Connect to mongo
-mongoose
-    .connect(db, {useNewUrlParser: true, useCreateIndex: true}) //adding a new mongo url parser
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
+// Middlewares for every request
+mongoose.connect("mongodb://localhost:27017/db-relations", {useNewUrlParser: true}, () => {
+    console.log("[o] Connected to the DB")
+})
 
-//Use Routes
-
-
-
-const PORT = process.env.PORT || 5000
+// Routes
+app.use('/', require('./routes/userRoutes.js'))
 
 
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    if (err.name === "UnauthorizedError") {
+        // express-jwt gives the 401 status to the err object for us
+        res.status(err.status);
+    }
+    return res.send({errMsg: err.message})
+})
 
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// Server
+app.listen(PORT, () => {
+    console.log(`[+] Server is running on port ${PORT}` )
+})
