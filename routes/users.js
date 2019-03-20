f2c39d59c55c1e7593eecb037d19038b4c34e1af
Ani Turner <aniturner93@gmail.com>
2019-03-19 19:38:44 -0600

    changed schema name

--- routes/users.js
+++ routes/users.js
@@ -1,62 +1,25 @@
-const express = require('express')
-const router = express.Router();
-const bcrypt = require('bcrypt.js')
-const config = require('config')
-const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-//User Model
-const User = require('../../models/User')
 
-router.post('/', (req, res) => {
-    const {username, password} = req.body
+//create Schema
+const userSchema = new Schema ({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
 })
 
-    //Validation
-    if(!username || !password) {
-        return res.status(400).json({msg: "please enter all fields"})
-    }
 
-    //check for existing user
-    User.findOne({username}).then(user => {
-        if(user) {
-            return res.status(400).json({msg: 'User already exists'})
-
-            //if user doesn't exist - create one
-            const newUser = new User({
-                username,
-                password
-            })
-
-            //crete salt and hash
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if(err) throw err;
-                    newUser.password = hash
-                    newUser.save()
-                    .then(user => {
-                        //payload
-                        jwt.sign(
-                            {id: user.id},
-                            config.get('jwtSecret'),
-                            {expiresIn: 3600},
-                            (err, token) => {
-                                if(err) throw err;
-                                res.json({
-                                    token,
-                                    user: {
-                                        id: user.id,
-                                        username: user.username
-                                    }
-                                })
-                            }
-                        )
-                       
-                    })
-                })
-            } )
-        }
-    }
-
-
-
-module.exports = router
+module.exports = mongoose.model('user', userSchema)
